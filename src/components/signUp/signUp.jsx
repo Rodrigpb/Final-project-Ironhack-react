fde586082c31d59305008dcb424d4a44a547d548
Rodrigo Poblacion <rodrigopoblacion@gmail.com>
2020-11-04 21:44:17 +0100

    Gran día

--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import FormUserDetails from './formUserDetails'
 import FormBusinessDetails from './formBusinessDetails'
+import Congratulations from './congratulations';
 import { useAuthContext } from '../../contexts/AuthContext';
+import {createUser} from '../../services/api.service';
 
+const validations = {
+	firstName: (value) => value.length > 1,
+	lastName:  (value) => value.length > 1,
+	email: (value) =>{
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(value).toLowerCase());
+  },
+	password: (value) => value.length > 1,
+	number: (value) =>{
+    var str = value.toString().replace(/\s/g, '');
+    return str.length === 9 && /^[679]{1}[0-9]{8}$/.test(str);
+}
+};
 
 export default class SignUp extends React.Component {
  
   state= {
-      data : {
+    data : {
         step: 1,
         firstName: '',
         lastName: '',
@@ -19,39 +34,27 @@ export default class SignUp extends React.Component {
         razonSocial: '',
         direccion: ''
     },
-      error: {
+    error: {
         firstName: true,
         lastName: true,
         email: true,
-        password: true
+        password: true,
+        number: true,
       },
-      touch: {}
+    touch: {}
   }
-
-  
-  
 
 nextStep = () => {
   const { step } = this.state.data;
   this.setState({
-    step: step + 1
-  });
+    data: { ...this.state.data, step: step + 1 },
+    });
 };
 
 prevStep = () => {
   const { step } = this.state.data;
   this.setState({
-    step: step - 1
-  });
-};
-
-handleChange = (e) => {
-  const { name, value } = e.target;
-  
-  this.setState({
-    data: { ...this.state.data, [name]: value },
-    error: {},
-    touch:{}
+    data: { ...this.state.data, step: step - 1 },
     });
 };
 
@@ -60,19 +63,58 @@ handleSubmit = (e) => {
   console.log("Hola")
 
   const newUser = async () => {
-    
-  }
+    try {
+      const user = await createUser(this.state.data)
+      console.log(user)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  };
+  newUser();
 }
+
+handleChange = (e) => {
+  const { name, value } = e.target;
+  const isValid = validations.hasOwnProperty(name) ? validations[name](value) : '';
+  
+  this.setState({
+    data: { ...this.state.data, [name]: value },
+    error: {
+      ...this.state.error,
+        [name]: isValid !== '' && !isValid
+    },
+    touch:{
+      ...this.state.touch
+    }
+    });
+    console.log(this.state.error);
+};
+
+handleBlur = (e) => {
+  const { name } = e.target;
+   this.setState({
+     ...this.state,
+     touch: {
+       ...this.state.touch,
+       [name]: true
+     }
+   });
+};
+
+
 
 render() {
     const { step } = this.state.data;
-
+    const isError = Object.values(this.state.error).some((el) => el);
+    console.log(isError)
     switch (step) {
       case 1:
         return (
           <FormUserDetails
             nextStep={this.nextStep}
             handleChange={this.handleChange}
+            handleBlur={this.handleBlur}
             value={this.state}
           />
         );
@@ -82,10 +124,16 @@ render() {
             nextStep={this.nextStep}
             prevStep={this.prevStep}
             handleChange={this.handleChange}
+            handleBlur={this.handleBlur}
             handleSubmit={this.handleSubmit}
             value={this.state}
+            disable={isError}
           />
         );
+        case 3:
+          return (
+            <Congratulations/>
+          )
       default:
         return null
     }
